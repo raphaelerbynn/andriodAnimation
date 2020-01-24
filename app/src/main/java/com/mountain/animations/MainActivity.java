ea49c6ddfbaf37bc819372264b5fed8f142fe988
raphaelerbynn <erbynn1234@gmail.com>
2020-01-24 09:50:43 +0000

    more animation options

--- app/src/main/java/com/mountain/animations/MainActivity.java
+++ app/src/main/java/com/mountain/animations/MainActivity.java
@@ -3,17 +3,45 @@ package com.mountain.animations;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    public void fade(View view){
+    public void fade(View view) {
         ImageView up = findViewById(R.id.up);
-        up.animate().alpha(0f).setDuration(4000);
+        ImageView up2 = findViewById(R.id.up2);
+
+        if (up.getAlpha() == 1) {
+
+            up.animate().alpha(0f).setDuration(2000);
+//            up2.setVisibility(View.VISIBLE);
+
+            up2.animate().alpha(1f).setDuration(2000);
+            up2.animate().rotation(1800f).setDuration(4000);
+//        up.setVisibility(View.INVISIBLE);
+        }
+        if (up2.getAlpha() == 1){
+
+            up2.animate().alpha(0f).setDuration(2000);
+            up.animate().alpha(1f).setDuration(2000);
+
+            up.animate().rotation(1800f).setDuration(4000);
+        }
+
+
+
     }
+
+    public void movePic(View view){
+        ImageView currency = findViewById(R.id.currency);
+
+        currency.animate().translationYBy(100f).setDuration(2000);
+//        currency.animate().translationZ(500f).setDuration(2000);
+//        currency.animate().translationXBy(100f).setDuration(2000);
+    }
+
 
 
 
@@ -24,5 +52,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView currency = findViewById(R.id.currency);
+
+        currency.animate().translationY(-100);
     }
 }
